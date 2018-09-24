6af3c39e23a5a3a409bb04443360c0f66bf9be94
Tomas Polesovsky <xpoleso1@nymfe35.fi.muni.cz>
2018-09-24 16:31:44 +0200

    Added new feature

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -26,6 +27,11 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 		List<Car> carsOfSameType = allCars.get(referenceCar.getType());
 		return carsOfSameType.stream().filter(car -> referenceCar.getConstructionYear() == car.getConstructionYear()
 				&& car.getPrice() < referenceCar.getPrice()).collect(Collectors.toList());
+	}
+
+	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		CarShopStorage.getInstancce().getCars().remove(car);
 	}
 
 	@Override
